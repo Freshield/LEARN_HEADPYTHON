13c62087f294763765a6d323edaa815f699195f4
freshield <zxdsw199182@gmail.com>
2017-08-19 18:58:25 -0400

    done chp6

---
+++ Review/Chp5/test4.py
@@ -0,0 +1,5 @@
+distance = set()
+
+distance = {10.6, 11, 8, 10.6, 'two', 7}
+
+print(distance)
---
+++ Review/Chp5/test5.py
@@ -0,0 +1,20 @@
+dic = {'ja':'james.txt', 'ju':'julie.txt', 'mi':'mikey.txt', 'sa':'sarah.txt'}
+
+def sanitize(time_string):
+    if '-' in time_string:
+        splitter = '-'
+    elif ':' in time_string:
+        splitter = ':'
+    else:
+        return time_string
+
+    mins, secs = time_string.split(splitter)
+    return mins + '.' + secs
+
+for key,value in dic.items():
+    with open(value) as file:
+        data = file.readline()
+        data = data.strip().split(',')
+        clean_data = sorted(set([sanitize(item) for item in data]))[0:3]
+        print(clean_data)
+
