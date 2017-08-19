a583aec61a9ef422a5ca4f8707d8487dabf3f6e7
freshield <zxdsw199182@gmail.com>
2017-08-18 21:18:40 -0400

    done change type

--- Review/Chp5/test1.py
+++ Review/Chp5/test1.py
@@ -1,7 +1,20 @@
 dic = {'ja':'james.txt', 'ju':'julie.txt', 'mi':'mikey.txt', 'sa':'sarah.txt'}
 
+def sanitize(time_string):
+    if '-' in time_string:
+        splitter = '-'
+    elif ':' in time_string:
+        splitter = ':'
+    else:
+        return time_string
+
+    mins, secs = time_string.split(splitter)
+    return mins + '.' + secs
+
 for key,value in dic.items():
     with open(value) as file:
         data = file.readline()
         data = data.strip().split(',')
-        print(data)
+        clean_data = [sanitize(item) for item in data]
+        print(sorted(clean_data))
+
---
+++ Review/Chp5/test2.py
@@ -0,0 +1,14 @@
+data = [6,3,1,2,4,5]
+
+print(data)
+
+data.sort()
+print(data)
+
+data = [6,3,1,2,4,5]
+
+print(data)
+
+data2 = sorted(data)
+print(data)
+print(data2)
