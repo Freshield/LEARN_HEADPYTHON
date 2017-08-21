415d11afc172738b7e0cee7e97be9ad8e54526ed
freshield <zxdsw199182@gmail.com>
2017-08-20 20:52:24 -0400

    done chp6

---
+++ Review/Chp6/test7.py
@@ -0,0 +1,26 @@
+class AtheleteList(list):
+    def __init__(self,a_name,a_dob=None,a_times=[]):
+        list.__init__([])
+        self.name = a_name
+        self.dob = a_dob
+        self.extend(a_times)
+
+    def sanitize(self,time_string):
+        if '-' in time_string:
+            splitter = '-'
+        elif ':' in time_string:
+            splitter = ':'
+        else:
+            return time_string
+
+        mins, secs = time_string.split(splitter)
+        return mins + '.' + secs
+
+    def top3(self):
+        return sorted(set([self.sanitize(t) for t in self]))[0:3]
+
+vera = AtheleteList('vera')
+vera.append('1.31')
+print(vera.top3())
+vera.extend(['2.22','1-21','2:22'])
+print(vera.top3())
